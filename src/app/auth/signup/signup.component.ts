009bc4e05578e72447815876b40d3409b387737d
Jacob Rose <jacobdanielrose@gmail.com>
2021-07-26 17:52:19 +0200

    getting project set up

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
+  // TODO: take care of this??
+  maxDate: Date = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
+    this.maxDate = new Date();
+  }
+
+  onSubmit(form: NgForm) {
+
   }
 
 }
---
+++ src/app/material.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core'
+import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ]
+})
+export class MaterialModule {}
