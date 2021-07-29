4d66f582d3771a767e9c152da7a4271d5e821032
romulus <jacobdanielrose@gmail.com>
2021-07-29 14:48:52 +0200

    connected firebase and integrated database and user authentication.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDW2FSxT9q4RBHWvFvrom9ougue_FfmwC4",
+    authDomain: "ng-fitness-tracker-d4705.firebaseapp.com",
+    projectId: "ng-fitness-tracker-d4705",
+    storageBucket: "ng-fitness-tracker-d4705.appspot.com",
+    messagingSenderId: "386038445760",
+    appId: "1:386038445760:web:39dfb08fc9d64822555f25",
+    measurementId: "G-Y7BDD8H4KK"
+  }
 };
 
 /*
